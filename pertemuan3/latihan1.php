b034de74142edb54e475ee7ed54e9b2852ba324f
hapidzfadli <hapidzfadli@gmail.com>
2021-02-12 21:42:59 +0700

    pertemuan 3 : belajar struktur kendali (pengulangan dan percabangan)

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,34 @@
+<?php 
+//Pengulangan
+
+//For
+// while
+// do..while
+//foreach
+
+//For
+// for($i = 0; $i < 5; $i++){
+//     echo 'Hello World <br>';
+// };
+
+// while
+// $i = 0;
+// while( $i < 5 ){
+//     echo 'Hello World <br>';
+//     $i++;
+// }
+
+
+// do..while
+
+// $i = 0;
+// do {
+//     echo 'Hello World! <br>';
+
+// $i++;
+// } while( $i < 5);
+
+
+
+
+?>
